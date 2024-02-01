987e23678919c18bf836a577fa12da48a95e015a
Mihail-2020 <mihailorenikov06440@gmail.com>
2024-02-01 22:55:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// Пример ад колбэков(вложеные колбэки) 
+
+console.log('Request data...');
+/*
+setTimeout(() => {
+    console.log('Preparing data...');
+
+    const backendData = {
+        server: 'aws',
+        port: 2000,
+        status: 'working'
+    }
+
+    setTimeout(() => {
+        backendData.modified = true;
+        console.log('Data received', backendData)
+    }, 2000)
+}, 2000)
+ */
+
+const p = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        console.log('Preparing data...');
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'wokring'
+        }
+        resolve(backendData);// Говорим своему прмису что он завершил свое выполнение
+    }, 2000);
+});
+
+p.then(data => {
+    const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true;
+            resolve(data)
+        }, 2000)
+    })
+
+
+    p2.then(clientData => {
+        console.log('Data received', clientData);
+    })
+})
+
